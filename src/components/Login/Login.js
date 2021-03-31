6e89aaa74127307741cd6b4f495541dcc26bfd7c
snapper <zihanjashim2121@gmail.comm>
2021-04-01 00:14:46 +0600

    full stack client

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import './Login.css';
+import {Form , Button } from 'react-bootstrap';
+import { FaGoogle,FaFacebook } from 'react-icons/fa';
 
 const Login = () => {
     return (
         <div>
-            <h1>This is login</h1>
+            <div className="form">
+                <h4><strong>Login</strong></h4>
+            <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+
+                <Button variant="outline-dark" className="submitBtn" type="submit"> Login </Button>
+            </Form>
+            </div>
+            <div className="buttons">
+                <Button variant="outline-dark"> <FaGoogle /> Continue With Google </Button>
+                <Button variant="outline-dark"> <FaFacebook /> Continue With Facebook </Button>
+            </div>
         </div>
     );
 };
