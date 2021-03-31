8a6cf78fdccab82de49d9f812ef1238cadd471f4
snapper <zihanjashim2121@gmail.comm>
2021-03-31 23:56:27 +0600

    full stack client

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './Navigation.css';
 import { Link } from "react-router-dom";
+import {Navbar,Nav,Button} from 'react-bootstrap';
 
 const Navigation = () => {
     return (
         <div>
-            <div className="navigation">
-                <ul>
-                    <li><Link to="/login">Login</Link></li>
-                    <li><Link to="/admin">Admin</Link></li>
-                    <li><Link to="/order">Order</Link></li>
-                    <li><Link to="/">Home</Link></li>
-                </ul>
+            <div>
+            <Navbar className="navigation" bg="light" variant="light">
+                <Navbar.Brand href="/">Navbar</Navbar.Brand>
+                <Nav className="mr-auto">
+                <Nav.Link><Link to="/">Home</Link></Nav.Link>
+                <Nav.Link><Link to="/order">Order</Link></Nav.Link>
+                <Nav.Link><Link to="/admin">Admin</Link></Nav.Link>
+                </Nav>
+                <Button variant="outline-info"><Link to="/login">Login</Link></Button>
+            </Navbar>
             </div>
-
         </div>
     );
 };
