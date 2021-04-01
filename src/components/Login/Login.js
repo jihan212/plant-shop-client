e258e303f733f2137739cf3668a905cd56c307d4
snapper <zihanjashim2121@gmail.comm>
2021-04-01 09:20:06 +0600

    full-stack-client

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,8 +2,42 @@ import React from 'react';
 import './Login.css';
 import {Form , Button } from 'react-bootstrap';
 import { FaGoogle,FaFacebook } from 'react-icons/fa';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+    }
 
 const Login = () => {
+
+    const handleGoogleLogin = () => {
+        
+        const provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+            /** @type {firebase.auth.OAuthCredential} */
+            var credential = result.credential;
+
+            // This gives you a Google Access Token. You can use it to access the Google API.
+             var token = credential.accessToken;
+             // The signed-in user info.
+            var user = result.user;
+            // ...
+        }).catch((error) => {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
+    }
+
     return (
         <div>
             <div className="form">
@@ -23,7 +57,7 @@ const Login = () => {
             </Form>
             </div>
             <div className="buttons">
-                <Button variant="outline-dark"> <FaGoogle /> Continue With Google </Button>
+                <Button onClick={handleGoogleLogin} variant="outline-dark"> <FaGoogle /> Continue With Google </Button>
                 <Button variant="outline-dark"> <FaFacebook /> Continue With Facebook </Button>
             </div>
         </div>
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAjmSGzXDS2HTZoYb4s9FURk1Y0PML3poc",
+    authDomain: "green-point-a677d.firebaseapp.com",
+    projectId: "green-point-a677d",
+    storageBucket: "green-point-a677d.appspot.com",
+    messagingSenderId: "616486631490",
+    appId: "1:616486631490:web:3edbf2d3587896386815db"
+  };
+
+export default firebaseConfig;
