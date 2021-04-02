2cf0a94edea17102634e19013bd3fe1ee5daa6fc
snapper <zihanjashim2121@gmail.comm>
2021-04-02 22:32:36 +0600

    full-stack-client

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,24 +2,17 @@ import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
 
 const Home = () => {
-    const [loading , setLoading] = useState(true);
     const [products,setProducts] = useState([]);
     useEffect(() =>{
         fetch('https://secure-brook-07656.herokuapp.com/products')
         .then(res => res.json())
         .then(data => setProducts(data))
-        setLoading(false);
     },[])
     return (
         <div>
-            <div style={{display: loading? "block" : "none"}}>
-                <h1>Loading images</h1>
-            </div>
-            <div style={{display: loading? "none" : "block"}}>
             {
                 products.map((product,i) => <Product key={i} product={product}></Product>)
             }
-            </div>
         </div>
     );
 };
